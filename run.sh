58e29493732e313c3f8ab0bc05b92799c55cd5d0
Hendry William <hendriwilliam29@gmail.com>
2026-02-24 14:26:04 +0700

    Add sidecar pattern

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+# kg pods --namespace=default
+# kg pods/sidecar-xxxx-xxx -o yaml (return its CURRENT state.)
 alias kg='kubectl get'
+# kd pods/haproxy
 alias kd='kubectl describe'
+# krm pods --namespace=default / krm -f k8sobjectfile.yaml --now
 alias krm='kubectl delete'
+# ke explain pod.metadata / ke explain pod.spec.restartPolicy
+# ke explain objects (return its INITIAL state)
+alias ke='kubectl explain'
+# ka -f kubernetestobjectfile.yaml
 alias ka='kubectl apply'
 alias kgetctx='kubectl config view --minify | grep namespace:'
+# ksetctx kube-system / ksetctx default
 alias ksetctx='kubectl config set-context --current --namespace'
+# kex -it mypod -c sidecar -- touch /tmp/crash
+alias kex='kubectl exec'
+# klog pods/mypod -c mycontainer
+alias klog='kubectl logs'
