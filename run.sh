af8b79ddf5ab5a7935b23ad6d05ec48e466d97ac
Hendry William <hendriwilliam29@gmail.com>
2026-02-26 17:05:09 +0700

    WIP moltis

--- run.sh
+++ run.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# kg pods --namespace=default
-# kg pods/sidecar-xxxx-xxx -o yaml (return its CURRENT state.)
 alias kg='kubectl get'
-# kd pods/haproxy
 alias kd='kubectl describe'
-# krm pods --namespace=default / krm -f k8sobjectfile.yaml --now
 alias krm='kubectl delete'
-# ke explain pod.metadata / ke explain pod.spec.restartPolicy
-# ke explain objects (return its INITIAL state)
 alias ke='kubectl explain'
-# ka -f kubernetestobjectfile.yaml
 alias ka='kubectl apply'
 alias kgetctx='kubectl config view --minify | grep namespace:'
-# ksetctx kube-system / ksetctx default
 alias ksetctx='kubectl config set-context --current --namespace'
-# kex -it mypod -c sidecar -- touch /tmp/crash
-alias kex='kubectl exec'
-# klog pods/mypod -c mycontainer
+alias kexec='kubectl exec'
 alias klog='kubectl logs'
+alias kapires='kubectl api-resources | more'
+alias kgetcfg='kubectl config view'
